9623f4c473677fcf620157c9603cfb1fc0cc9063
Natasha <nnkonovalova@bk.ru>
2023-03-15 02:03:35 +0300

    add new img, iconic

--- store/shop/urls.py
+++ store/shop/urls.py
@@ -12,6 +12,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from landing import views
@@ -19,3 +21,5 @@ from landing import views
 urlpatterns = [
     url(r'^shop123/', views.landing, name='shop'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- store/store/settings.py
+++ store/store/settings.py
@@ -122,8 +122,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'store/static/'
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static')
 ]
