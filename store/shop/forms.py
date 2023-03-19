e2737ad065d2b2e7b6fa6f3664b9de872372d41b
Natasha <nnkonovalova@bk.ru>
2023-03-20 00:55:14 +0300

    added new files

---
+++ store/shop/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.IntegerField(min_value=1)
+    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
