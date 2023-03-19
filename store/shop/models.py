3304fcf1e7ffc08d25074468da20f59fe79e668d
Natasha <nnkonovalova@bk.ru>
2023-03-20 00:58:58 +0300

    new html for cart, users and corrected links

--- store/shop/models.py
+++ store/shop/models.py
@@ -1,11 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images', blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+    
+
+    
+class Cart(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s cart"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in {self.cart}"
--- store/shop/views.py
+++ store/shop/views.py
@@ -1,46 +1,122 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Q
+from .models import Product, Order, OrderItem
+from .forms import OrderForm
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+
 def shop(request):
-
     return render(request, 'shop/shop.html')
 
 def product_list_1(request):
-    #products = Product.objects.all()
-    #context = {'products': products}
-    return render(request, 'shop/products_list_1.html')
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    else:
+        products = Product.objects.all()
+    return render(request, 'shop/products_list_1.html', {'products': products})
 
 def product_list_2(request):
-    #products = Product.objects.all()
-    #context = {'products': products}
-    return render(request, 'shop/products_list_2.html')
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    else:
+        products = Product.objects.all()
+    return render(request, 'shop/products_list_2.html', {'products': products})
 
 def product_list_3(request):
-    #products = Product.objects.all()
-    #context = {'products': products}
-    return render(request, 'shop/products_list_3.html')
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    else:
+        products = Product.objects.all()
+    return render(request, 'shop/products_list_3.html', {'products': products})
 
-def product_detail(request, pk):
-    #product = Product.objects.get(pk=pk)
-    #context = {'product': product}
-    return render(request, 'shop/products_detail.html')
 
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'shop/product_detail.html', {'product': product})
+
+@login_required
 def cart_detail(request):
-    context = {}
-    return render(request, 'shop/cart_detail.html')
+    order = Order.objects.filter(user=request.user, ordered=False).first()
+    if order:
+        order_items = order.items.all()
+    else:
+        order_items = []
+    return render(request, 'shop/cart_detail.html', {'order_items': order_items, 'order': order})
 
+@login_required
 def account_detail(request):
-    context = {}
     return render(request, 'shop/account_detail.html')
 
-def login(request):
-    context = {}
-    return render(request, 'shop/login.html')
-
 def signup(request):
-    context = {}
-    return render(request, 'shop/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('shop:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'shop/signup.html', {'form': form})
 
-def checkout(request):
-    context = {}
-    return render(request, 'shop/checkout.html')
+@login_required
+def order_create(request):
+    order = Order.objects.filter(user=request.user, ordered=False).first()
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            new_order = form.save(commit=False)
+            new_order.user = request.user
+            new_order.order = order
+            new_order.save()
+            for item in order.items.all():
+                new_item = OrderItem.objects.create(order=new_order, product=item.product, quantity=item.quantity)
+                new_item.save()
+            order.ordered = True
+            order.save()
+            return redirect('shop:index')
+    else:
+        form = OrderCreateForm()
+    return render(request, 'shop/checkout.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('shop:index')
+        else:
+            return render(request, 'shop/login.html', {'error': 'Invalid login credentials'})
+    else:
+        return render(request, 'shop/login.html')
+    
+def user_logout(request):
+    logout(request)
+    return redirect('shop:index')
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart_detail')
