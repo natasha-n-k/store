ca1476fb455be0622beba3d9ec4926e25919cee0
Natasha <nnkonovalova@bk.ru>
2023-03-22 19:10:17 +0300

    registration models in admin

--- store/shop/admin.py
+++ store/shop/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product, Order, OrderItem, Cart, CartItem
 
-# Register your models here.
+# регистрация модели Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+# регистрация модели Order
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'paid']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
+
+# регистрация модели OrderItem
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+# регистрация модели Cart
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user']
+
+# регистрация модели CartItem
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity', 'cart']
