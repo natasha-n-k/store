5241fa767c77a128b9bce419596806a0276bb715
Natasha <nnkonovalova@bk.ru>
2023-03-22 19:23:33 +0300

    changed admin

--- store/shop/admin.py
+++ store/shop/admin.py
@@ -6,17 +6,17 @@ from .models import Product, Order, OrderItem, Cart, CartItem
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price']
 
+# регистрация модели OrderItem
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
 # регистрация модели Order
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'paid']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
-
-# регистрация модели OrderItem
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    raw_id_fields = ['product']
 
 # регистрация модели Cart
 @admin.register(Cart)
