617df4525ed39ec1b99034b3e3f6664a2d52402c
Natasha <nnkonovalova@bk.ru>
2023-04-17 22:22:47 +0300

    corrected style product_detail

--- store/shop/models.py
+++ store/shop/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product_images', storage=fs)
     color = models.CharField(max_length=50, default='white', null=True)
-    size = models.CharField(max_length=10, null=True)
+    size = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.name
